526bcdb79d79f859213c756ef85f5b593652fa89
rohan <rohansenkesi@gmail.com>
2026-09-04 20:28:08 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
-    connection.rcv(1024)
+    connection.recv(1024)
     connection.sendall(b"+PONG\r\n")
 
 
